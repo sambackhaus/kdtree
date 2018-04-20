b31ae60a1ddb25eedd9ee02f88e7ec84411ac2ab
Sam Backhaus <sam.backhaus@otto.de>
2018-04-20 15:23:02 +0200

    did fancy stuff

--- src/main/scala/de/gwik/similarity/NmslibQuery.scala
+++ src/main/scala/de/gwik/similarity/NmslibQuery.scala
@@ -1,12 +1,12 @@
 package de.gwik.similarity
 
-import edu.cmu.lti.oaqa.similarity.{QueryService, ReplyEntry}
-import org.apache.thrift.TException
+import edu.cmu.lti.oaqa.similarity.QueryService
 import org.apache.thrift.protocol.TBinaryProtocol
 import org.apache.thrift.transport.TSocket
+
 import scala.io.Source
 import scala.collection.JavaConversions._
-import sys.process._
+import scala.sys.process._
 
 class NmslibQuery(dataUrl: String) extends GenericQuery(dataUrl) with DataConfig {
 
@@ -15,32 +15,39 @@ class NmslibQuery(dataUrl: String) extends GenericQuery(dataUrl) with DataConfig
   val dim: Int = testSequences.head.length
 
   val pathToQueryServer = "../nmslib/query_server/cpp_client_server/query_server"
-  val shellExecute = s"$pathToQueryServer -i ./target/test_data.tsv -s l2 -m sw-graph -c NN=10 efConstruction=200 -p 10000 &&"
+  val shellExecute = s"$pathToQueryServer -i ./target/test_data.tsv -s l2 -m sw-graph -c NN=10 efConstruction=200 -p 10000"
   println(shellExecute)
-  shellExecute !
+  val nmsLibServerProcess = shellExecute run
 
-  val transport = new TSocket("localhost", 10000)
-  transport.open()
-  val protocol = new  TBinaryProtocol(transport)
+  var isConnected = false
+  var transport : Option[TSocket] = None
+  print("Connection to localhost:10000")
+  while(!isConnected) {
+    try {
+      transport = Some(new TSocket("localhost", 10000))
+      transport.get.open()
+      isConnected = true
+    } catch {
+      case e : Exception => {
+        Thread.sleep(1000)
+        print(".")
+      }
+    }
+  }
+  println("done!")
+
+  val protocol = new TBinaryProtocol(transport.get)
   val client = new QueryService.Client(protocol)
-
 
   override def queryNN(queryVector: Seq[Double], nearestNeighborCount: Int): Seq[QueryResult] = {
     val queryString = queryVector.mkString("   ")
 
-    try {
-      val result = client.knnQuery(nearestNeighborCount, queryString, true, true).toList
-      result.map(re => new QueryResult(queryVector,None, Option(re.getDist()), Option(re.getId().toString)))
-    } catch {
-      case (te : TException ) =>
-        println("Apache Thrift exception: " + te)
-        te.printStackTrace()
-        println("needs: \n ./query_server -i /home/chambroc/code/sandbox/out/test_data.tsv -s l2 -m sw-graph -c NN=10,efConstruction=200 -p 10000")
-        Seq()
-    }
+    val result = client.knnQuery(nearestNeighborCount, queryString, true, false).toList
+    result.map(re => new QueryResult(queryVector,None, Option(re.getDist()), Option(re.getId().toString)))
   }
 
-  def stopServer(): Unit = {
-    transport.close()
+  override def tearDown(): Unit = {
+    if(transport.isDefined) transport.get.close()
+    nmsLibServerProcess.destroy()
   }
 }
