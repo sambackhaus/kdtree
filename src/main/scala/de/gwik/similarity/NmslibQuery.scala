5fbc015ee6bbe3f9cbb5a227012bfc983efa884b
chambroc <christian.hambrock@otto.de>
2018-04-20 13:28:46 +0200

    ch| start cpp server via shell

--- src/main/scala/de/gwik/similarity/NmslibQuery.scala
+++ src/main/scala/de/gwik/similarity/NmslibQuery.scala
@@ -1,13 +1,12 @@
 package de.gwik.similarity
 
-import java.util.UUID
-
 import edu.cmu.lti.oaqa.similarity.{QueryService, ReplyEntry}
 import org.apache.thrift.TException
 import org.apache.thrift.protocol.TBinaryProtocol
 import org.apache.thrift.transport.TSocket
 import scala.io.Source
 import scala.collection.JavaConversions._
+import sys.process._
 
 class NmslibQuery(dataUrl: String) extends GenericQuery(dataUrl) with DataConfig {
 
@@ -15,10 +14,16 @@ class NmslibQuery(dataUrl: String) extends GenericQuery(dataUrl) with DataConfig
   val testSequences: Seq[Seq[Double]] = src.map(l => l.split("   ").map(c => c.toDouble  ).toSeq).toSeq
   val dim: Int = testSequences.head.length
 
+  val pathToQueryServer = "../nmslib/query_server/cpp_client_server/query_server"
+  val shellExecute = s"$pathToQueryServer -i ./target/test_data.tsv -s l2 -m sw-graph -c NN=10 efConstruction=200 -p 10000 &&"
+  println(shellExecute)
+  shellExecute !
+
   val transport = new TSocket("localhost", 10000)
   transport.open()
   val protocol = new  TBinaryProtocol(transport)
   val client = new QueryService.Client(protocol)
+
 
   override def queryNN(queryVector: Seq[Double], nearestNeighborCount: Int): Seq[QueryResult] = {
     val queryString = queryVector.mkString("   ")
